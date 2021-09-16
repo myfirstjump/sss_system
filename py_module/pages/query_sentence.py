784b6bac91383e662040dc038e30ed27e564ac57
edwardchen <myfirstjump@gmail.com>
2021-09-16 23:43:44 +0800

    test

--- py_module/pages/query_sentence.py
+++ py_module/pages/query_sentence.py
@@ -19,5 +19,5 @@ def final_query(arg):
 
 conn = pymssql.connect(host='localhost', user = 'myfirstjump', password='myfirstjump', database='STOCK_SKILL_DB')
 cursor = conn.cursor(as_dict=True)
-result = cursor.execute('SELECT * FROM STOCK_SKILL_DB.dbo.TW_STOCK_PRICE_Daily WHERE date > "2021-09-10" AND stock_id = "2330"')
+result = cursor.execute('SELECT * FROM STOCK_SKILL_DB.dbo.TW_STOCK_PRICE_Daily WHERE stock_id = "2330"')
 print(result)
