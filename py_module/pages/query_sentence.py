4d311e1b96d0e900891d3c0185739e8ed1a01ec1
edwardchen <myfirstjump@gmail.com>
2021-09-30 00:05:34 +0800

    mo

--- py_module/pages/query_sentence.py
+++ py_module/pages/query_sentence.py
@@ -289,7 +289,6 @@ def create_query_0401(days, period, buy_sell, direct, lot):
         sign = '>='
         sign_0 = '<='
         lot = lot * -1000
-    return sign, sign_0, lot
 
     query = '''
     (SELECT stock_id FROM
