8126ba90b4aba62b88cea27f8b4bb899fd057fbc
myfirstjump <myfirstjump@gmail.com>
2021-11-18 20:43:54 +0800

    m

--- py_module/pages/query_sentence.py
+++ py_module/pages/query_sentence.py
@@ -37,8 +37,8 @@ counter_holdrange_m = 'STOCK_Counter_DB.dbo.TW_STOCK_HOLDRANGE_monthly'
 def query_combine(query_dict, col_name_dict):
     query_number = len(query_dict)
 
-    specific_columns_string = " ,".join(['{}.{}'.format(alias, colname[1]) for colname,alias in zip(col_name_dict.items(), ascii_lowercase)])
-    remark_string = "+".join(['{}.remark'.format(i) for i in ascii_lowercase][:query_number]) + ","
+    specific_columns_string = " ,".join(['{}.{}'.format(alias, colname[1]) for colname,alias in zip(col_name_dict.items(), ascii_lowercase)]) + ","
+    remark_string = "+".join(['{}.remark'.format(i) for i in ascii_lowercase][:query_number])
     combined_query = "SELECT {}.stock_id, {}.stock_name, {}.industry_category, {}.type, {} {} Remark FROM ".format(ascii_lowercase[query_number], ascii_lowercase[query_number], 
     ascii_lowercase[query_number], ascii_lowercase[query_number], specific_columns_string, remark_string)
 
