860abd9856915fc4904a04765398eb9428ac1ecf
myfirstjump <myfirstjump@gmail.com>
2022-02-25 00:33:23 +0800

    m

--- py_module/data_processing.py
+++ py_module/data_processing.py
@@ -54,8 +54,9 @@ class DataProcessing(object):
         return data
     
     def iq_table_adjust(self, dataframe):
-
-        dataframe['date'] = dataframe['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+        
+        dataframe['date'] = dataframe['date'].dt.to_period('Q')
+        # dataframe['date'] = dataframe['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
         dataframe.set_index('date', inplace=True)
         print(dataframe)
         data_transposed = dataframe.T
