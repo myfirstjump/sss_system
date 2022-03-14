295a938b5945584ff2b9c5e6df5c57e12daceeed
myfirstjump <myfirstjump@gmail.com>
2022-03-14 17:32:31 +0800

    .

--- executor.py
+++ executor.py
@@ -1972,10 +1972,14 @@ def iq_interactive(stock_string, btn, stored_stock_id):
                                                 columns = [{"name": i, "id": i, "type": 'numeric', "format":Format().group(True)} for i in data_01_03.columns],
                                                 data=data_01_03.to_dict('records'),
                                                 style_cell={
-                                                    'minWidth': '180px', 'width': '180px', 'maxWidth': '180px', 'color':'white', 'background-color': '#212130', 'margin': '0', 'display': 'block',
+                                                    'minWidth': '180px', 'width': '180px', 'maxWidth': '180px', 'color':'white', 'background-color': '#212130',
                                                 },
                                                 style_header={
                                                     'textAlign':'center',
+                                                },
+                                                style_table={
+                                                    'margin': '0',
+                                                    'display': 'block',
                                                 }
                                             ),
                                         ]
