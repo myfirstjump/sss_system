4c5475f4d8dfd0b6f0aa8080581ddd6e8369025d
myfirstjump <myfirstjump@gmail.com>
2022-03-14 18:00:22 +0800

    .

--- executor.py
+++ executor.py
@@ -1963,7 +1963,7 @@ def iq_interactive(stock_string, btn, stored_stock_id):
                                                     'textAlign':'center',
                                                 },
                                                 style_table={
-                                                     'margin':'0.5%',
+                                                     'padding-left':'0.5%',
                                                 },
                                             ),
                                         ]
@@ -1981,7 +1981,7 @@ def iq_interactive(stock_string, btn, stored_stock_id):
                                                     'textAlign':'center',
                                                 },
                                                 style_table={
-                                                     'padding':'0.5%',
+                                                     'padding-left':'0.5px',
                                                 },
                                             ),
                                         ]
