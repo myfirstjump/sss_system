583adcef3b830e15e1e1f4c7fead1ce10818ce68
myfirstjump <myfirstjump@gmail.com>
2022-06-20 20:49:44 +0800

    update tw_stock_info

--- everyday_executor.bat
+++ everyday_executor.bat
@@ -1,3 +1,3 @@
 call C:\Users\myfir\anaconda3\Scripts\activate.bat C:\Users\myfir\anaconda3\envs\stock
 cd C:\Users\myfir\Documents\Repository\sss_system\schedule_scripts
-python stock_crawl.py
+python stock_everyday.py
--- py_module/config.py
+++ py_module/config.py
@@ -8,7 +8,7 @@ class Configuration(object):
         ###
 
         self.data_folder = ".\\data"
-        self.taiwan_stock_info = "111-02-05_TaiwanStockInfo.csv"
+        self.taiwan_stock_info = "111-06-20_TaiwanStockInfo.csv"
         self.log_folder = ".\\data\\log\\log.csv"
 
         self.dash_port = '8050'
