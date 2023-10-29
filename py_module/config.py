2a2bc866fe9ffa9693b0e3d406f89d4f53a82cac
myfirstjump <myfirstjump@gmail.com>
2023-10-29 19:51:21 +0800

    .

--- py_module/config.py
+++ py_module/config.py
@@ -8,7 +8,7 @@ class Configuration(object):
         ###
 
         self.data_folder = ".\\data"
-        self.taiwan_stock_info = "111-06-20_TaiwanStockInfo.csv"
+        self.taiwan_stock_info = "112-10-29_TaiwanStockInfo.csv"
         self.log_folder = ".\\data\\log\\log.csv"
 
         self.dash_port = '8050'
